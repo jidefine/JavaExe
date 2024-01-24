a600e69668a1f8c8f28fb8829e442057c95cc2c7
jidefine <youjio2000@naver.com>
2024-01-24 16:33:18 +0900

    산술연산자

--- Ch02_Variable/src/ch02/operator02/ArithOp.java
+++ Ch02_Variable/src/ch02/operator02/ArithOp.java
@@ -4,7 +4,13 @@ public class ArithOp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int num0 = 10;
+		int num1 = 3;
+		System.out.println(num0+num1); // 덧셈
+		System.out.println(num0-num1); // 뺄셈
+		System.out.println(num0*num1); // 곱셈
+		System.out.println(num0/num1); // 몫
+		System.out.println(num0%num1); // 나머지
 	}
 
 }
