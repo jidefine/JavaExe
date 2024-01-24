9d9a93d6683ee1c336e48a55c0ad867ea6af9ce1
bitcamp1 <bitcamp1@bitcamp>
2024-01-24 11:27:43 +0900

    Ch01_HelloWorld 프로젝트 생성

---
+++ Ch01_HelloWorld/src/ch01/hello/Hello.java
@@ -0,0 +1,11 @@
+package ch01.hello; //package는 소속을 의미함
+
+import java.awt.datatransfer.SystemFlavorMap;
+
+public class Hello {
+//	프로그램에서 프로그램 시작부분을 main으로 정해놓았음
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		
+	}
+}
