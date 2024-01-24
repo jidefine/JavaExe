5c0d53933a436001402e90c2a8b3d0167bab0d6c
bitcamp1 <bitcamp1@bitcamp>
2024-01-24 12:35:15 +0900

    Hello, 대한민국으로 변경 Hello, NCP08 출력을 추가

--- Ch01_HelloWorld/src/ch01/hello/Hello.java
+++ Ch01_HelloWorld/src/ch01/hello/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("HelloWorld");
 		System.out.println("Hello, Java");
 		System.out.println("Hello, 대한민국");
-		System.out.println("Hello, NCP");
+		System.out.println("Hello, NCP08");
 		
 	}
 }
