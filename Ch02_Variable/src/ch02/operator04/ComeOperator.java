710c6962f953fd8f101c6f0ea4031e4a8e778017
jidefine <youjio2000@naver.com>
2024-01-24 16:49:35 +0900

    논리연산자

---
+++ Ch02_Variable/src/ch02/operator04/ComeOperator.java
@@ -0,0 +1,13 @@
+package ch02.operator04;
+
+public class ComeOperator {
+	private void maid() {
+		int num0 = 100;
+		int num1 = 10;
+		int num2 = 100;
+		
+		boolean bVal = num0 > num1;
+		System.out.println(bVal);
+
+	}
+}
