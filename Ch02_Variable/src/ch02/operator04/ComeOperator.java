9b4197bdee42e62d317526f852581d537661c092
jidefine <youjio2000@naver.com>
2024-01-24 16:54:38 +0900

    num0,1,2을 이용한 boolean

--- Ch02_Variable/src/ch02/operator04/ComeOperator.java
+++ Ch02_Variable/src/ch02/operator04/ComeOperator.java
@@ -6,8 +6,14 @@ public class ComeOperator {
 		int num1 = 10;
 		int num2 = 100;
 		
-		boolean bVal = num0 > num1;
-		System.out.println(bVal);
+		boolean bVal = num0 > num1;// 100>10
+		System.out.println(bVal); //true
+		bVal = num0 < num1; // 100<10
+		System.out.println(bVal); //false
+		bVal = num0 == num2; // 100 == 100
+		System.out.println(bVal); //true
+		bVal = num0 >= num2; // 100 >= 100
+		System.out.println(bVal); //true
 
 	}
 }
