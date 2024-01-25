18ab86eafe4d85bfb5f404474846b294de272d81
jidefine <youjio2000@naver.com>
2024-01-25 17:16:58 +0900

    주석 추가

--- Ch03_Control/src/ch03/for10/ForEx.java
+++ Ch03_Control/src/ch03/for10/ForEx.java
@@ -3,6 +3,8 @@ package ch03.for10;
 public class ForEx {
 
 	public static void main(String[] args) {
+		// 1~10까지 출력
+		// while문과 구조가 같다
 		int i =1;  // 초기값
 		for(;i<=10;) { // ;을 통해서 한눈에 볼 수 있도록 사용
 			System.out.println(i); // 실행
