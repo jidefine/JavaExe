9f37a92eb9206ae3004a27ef1ff2102c5877bb40
jidefine <youjio2000@naver.com>
2024-01-25 16:38:38 +0900

    while문 문제 3

---
+++ Ch03_Control/src/ch03/while08/Resolve3.java
@@ -0,0 +1,17 @@
+package ch03.while08;
+
+public class Resolve3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1; // 초기식(시작: 언제부터)
+		int sum = 0;
+		while(i<11) { // 조건식(종료: 언제까지)
+			sum+=i;
+			i++; // 증감식(변화: 얼마만큼)
+		}
+		System.out.print(sum);
+	}
+
+
+}
