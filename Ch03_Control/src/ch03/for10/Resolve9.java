1cf0c8f3db459ca69d38d8a1617821923c82f3b2
jidefine <youjio2000@naver.com>
2024-01-25 17:40:24 +0900

    문제 9

---
+++ Ch03_Control/src/ch03/for10/Resolve9.java
@@ -0,0 +1,15 @@
+package ch03.for10;
+
+public class Resolve9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i =10;  // 초기값
+		for(;i<=20;) { // ;을 통해서 한눈에 볼 수 있도록 사용
+			System.out.print(i + " "); // 실행
+			i++; // 증가
+		}
+
+	}
+
+}
