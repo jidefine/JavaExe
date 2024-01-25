fd1ea18a45c2474dac994dff5173a88526859bdb
jidefine <youjio2000@naver.com>
2024-01-25 17:20:04 +0900

    오류 수정

--- Ch03_Control/src/ch03/for10/ForEx1.java
+++ Ch03_Control/src/ch03/for10/ForEx1.java
@@ -2,10 +2,9 @@ package ch03.for10;
 
 public class ForEx1 {
 	public static void main(String[] args) {
-		int i =1;  // 초기값
 		
 		//1번만 실행 ; 조건(계속 비교); 실행이 끝나면 변화
-		for(int i =1;i<=10;i++) { // 
+		for(int i=1;i<=10;i++) { // 
 			System.out.println(i); // 실행
 			i++; // 증가
 		}
