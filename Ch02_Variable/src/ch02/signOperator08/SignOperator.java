76c75373b4ccb3baf4c016f20f5d3e989d51a29c
jidefine <youjio2000@naver.com>
2024-01-25 11:40:10 +0900

    부호 연산자

---
+++ Ch02_Variable/src/ch02/signOperator08/SignOperator.java
@@ -0,0 +1,15 @@
+package ch02.signOperator08;
+
+public class SignOperator {
+
+	public static void main(String[] args) {
+		// sign은 부호를 의미함(부호연산자)
+		int num = +100;
+		int num1 = -num;
+		int num2 = -num1;
+		System.out.println(num);
+		System.out.println(num1);
+		System.out.println(num2);
+	}
+
+}
