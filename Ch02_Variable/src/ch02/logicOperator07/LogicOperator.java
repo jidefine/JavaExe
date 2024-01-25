bed21d8c2724472aabd4aa794f100ea9f9549cbf
jidefine <youjio2000@naver.com>
2024-01-25 11:37:17 +0900

    논리연산자

---
+++ Ch02_Variable/src/ch02/logicOperator07/LogicOperator.java
@@ -0,0 +1,31 @@
+package ch02.logicOperator07;
+
+public class LogicOperator {
+
+	public static void main(String[] args) {
+		
+		boolean logic;
+		
+		// And 연산
+		logic = true && true;
+		System.out.println("true && true => " + logic);
+		logic = true && false;
+		System.out.println("true && true => " + logic);
+		// OR 연산
+		logic = true || true;
+		System.out.println("true || true => " + logic);
+		logic = true || false;
+		System.out.println("true || true => " + logic);
+		// Xor 연산
+		logic = true ^ true;
+		System.out.println("true ^ true => " + logic);
+		logic = true ^ false;
+		System.out.println("true ^ true => " + logic);
+		// Not 연산
+		logic = !false;
+		System.out.println("!false => " + logic);
+		logic = !true;
+		System.out.println("!true => " + logic);
+	}
+
+}
