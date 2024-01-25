e9ade17d80e8aef84d18da296dc1e264898a4b03
jidefine <youjio2000@naver.com>
2024-01-25 16:38:18 +0900

    while문 문제 1

---
+++ Ch03_Control/src/ch03/while08/Resolve1.java
@@ -0,0 +1,14 @@
+package ch03.while08;
+
+public class Resolve1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0; // 초기식(시작: 언제부터)
+		while(i<100) { // 조건식(종료: 언제까지)
+			i++; // 증감식(변화: 얼마만큼)
+			System.out.print(i + " "); // 실행문
+		}
+	}
+
+}
