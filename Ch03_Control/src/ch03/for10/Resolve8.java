b6fe2e4f5aa658ed7d26fac4233d444044848a99
jidefine <youjio2000@naver.com>
2024-01-25 17:40:17 +0900

    문제 8

---
+++ Ch03_Control/src/ch03/for10/Resolve8.java
@@ -0,0 +1,15 @@
+package ch03.for10;
+
+public class Resolve8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i =1;  // 초기값
+		for(;i<=100;) { // ;을 통해서 한눈에 볼 수 있도록 사용
+			System.out.print(i + " "); // 실행
+			i++; // 증가
+		}
+
+	}
+
+}
