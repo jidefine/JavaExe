b67f34729bbd361560e91ef1e292a9e861162b37
jidefine <youjio2000@naver.com>
2024-01-25 17:15:53 +0900

    클래스 이름 변경 및 for문 작성법 2

--- Ch03_Control/src/ch03/for10/ForEx1.java
+++ Ch03_Control/src/ch03/for10/ForEx1.java
@@ -1,14 +1,14 @@
 package ch03.for10;
 
-public class ForEx {
-
+public class ForEx1 {
 	public static void main(String[] args) {
 		int i =1;  // 초기값
-		for(;i<=10;) { // ;을 통해서 한눈에 볼 수 있도록 사용
+		
+		//1번만 실행 ; 조건(계속 비교); 실행이 끝나면 변화
+		for(int i =1;i<=10;i++) { // 
 			System.out.println(i); // 실행
 			i++; // 증가
 		}
-
 	}
 
 }
