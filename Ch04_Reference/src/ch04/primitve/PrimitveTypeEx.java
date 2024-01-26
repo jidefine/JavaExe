54177667c4022b046ac9d8d1a3f9d90d2c51a007
jidefine <youjio2000@naver.com>
2024-01-26 12:40:58 +0900

    PrimitveTypeEx 기본 타입 용량

--- Ch04_Reference/src/ch04/primitve/PrimitveTypeEx.java
+++ Ch04_Reference/src/ch04/primitve/PrimitveTypeEx.java
@@ -4,7 +4,7 @@ class PrimitveTypeEx {
 	public static void main(String[] args) {
 //		아래 변수는 Stack에 생성되엇다가
 //		main의 영역을 벗어나면 Stack에서 사라진다
-//		main
+//		지역변수
 		byte b = 10;
 		char ch = 'A';
 		short s = 10000;
@@ -13,9 +13,11 @@ class PrimitveTypeEx {
 		float f = 3.14f;
 		double d = 3.14;
 		System.out.printf("byte size : %dbit\n", Byte.SIZE);
-		System.out.printf("char size : %dbit\n", Char.SIZE);
+		System.out.printf("char size : %dbit\n", Character.SIZE);
 		System.out.printf("short size : %dbit\n", Short.SIZE);
-		System.out.printf("int size : %dbit\n", Int.SIZE);
+		System.out.printf("int size : %dbit\n", Integer.SIZE);
 		System.out.printf("long size : %dbit\n", Long.SIZE);
+		System.out.printf("float size : %dbit\n", Float.SIZE);
+		System.out.printf("double size : %dbit\n", Double.SIZE);
 	}
 }
