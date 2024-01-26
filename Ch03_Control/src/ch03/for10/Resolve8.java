66274991987c98915f66e1c32738141d82cadba4
jidefine <youjio2000@naver.com>
2024-01-26 10:54:07 +0900

    Resolve8 풀이방법 추가

--- Ch03_Control/src/ch03/for10/Resolve8.java
+++ Ch03_Control/src/ch03/for10/Resolve8.java
@@ -8,6 +8,9 @@ public class Resolve8 {
 		for(;i<=100;) { // ;을 통해서 한눈에 볼 수 있도록 사용
 			System.out.print(i + " "); // 실행
 			i++; // 증가
+			
+//		for(int i =1;i<=100;i++) { // ;을 통해서 한눈에 볼 수 있도록 사용
+//			System.out.print(i + " "); 
 		}
 
 	}
