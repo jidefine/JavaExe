d668e4cb7bf507f93d130672bc2ea15872883e1c
jidefine <youjio2000@naver.com>
2024-01-26 14:16:20 +0900

    풀다가 맘

--- Ch03_Control/src/ch03/continue11/Resovle3.java
+++ Ch03_Control/src/ch03/continue11/Resovle3.java
@@ -1,13 +1,26 @@
 package ch03.continue11;
 
-import java.security.DomainCombiner;
+import java.util.Scanner;
 
 public class Resovle3 {
 	public static void main(String[] args) {
+		
+		int money = 0;
 		System.out.println("--------------");
-		System.out.println("1.예금");
+		System.out.println("1.예금 2.출금 3.잔고 4.종료");
 		System.out.println("--------------");
-	for(int i=0;i<=5;i++)
-		System.out.println(i);
+		
+		while(true) {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("선택 >> ");
+			int num = sc.nextInt();
+			
+			if(num == 1) {
+				System.out.print("예금액 >> ");
+				money = sc.nextInt();
+			}
+			
+			
+		}
 	}
 }
