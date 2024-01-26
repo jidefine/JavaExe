f29f8adb9285bd02aa4f968fbdefe412aa121a72
jidefine <youjio2000@naver.com>
2024-01-26 17:50:13 +0900

    문제 1 해결

---
+++ Ch05_Array/src/ch05/array08/Resovle1.java
@@ -0,0 +1,22 @@
+package ch05.array08;
+
+import java.util.Arrays;
+
+public class Resovle1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] num= new int[5];
+		num[0]=1;
+		num[1]=2;
+		num[2]=3;
+		num[3]=4;
+		num[4]=5;
+		
+		Arrays.sort(num);
+		System.out.println(Arrays.toString(num));
+		System.out.println("최소값 : " + num[0]);
+		System.out.println("최대값 : " + num[4]);
+	}
+
+}
