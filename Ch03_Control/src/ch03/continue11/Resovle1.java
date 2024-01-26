d2b41dd44d4842b9a95f7dafc9506a9bc61e1a26
jidefine <youjio2000@naver.com>
2024-01-26 12:33:02 +0900

    수정

---
+++ Ch03_Control/src/ch03/continue11/Resovle1.java
@@ -0,0 +1,21 @@
+package ch03.continue11;
+
+import java.security.DomainCombiner;
+import java.util.Scanner;
+
+public class Resovle1 {
+	public static void main(String[] args) {
+//	for(int i=0;i<=5;i++)
+//		System.out.println(i);
+//	
+		int sum = 0;
+
+		for (int i = 1; i <= 100;) {
+			if (i % 3 == 0)
+				System.out.println(i);
+			sum += i;
+			i++; // 증감식(변화: 얼마만큼)
+		}
+		System.out.print("1~100까지의 정수 중에 3의 배수의 합은 " + sum);
+	}
+}
---
+++ Ch03_Control/src/ch03/continue11/Resovle3.java
@@ -0,0 +1,13 @@
+package ch03.continue11;
+
+import java.security.DomainCombiner;
+
+public class Resovle3 {
+	public static void main(String[] args) {
+		System.out.println("--------------");
+		System.out.println("1.예금");
+		System.out.println("--------------");
+	for(int i=0;i<=5;i++)
+		System.out.println(i);
+	}
+}
