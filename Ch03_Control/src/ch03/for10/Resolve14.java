0be044fa9abba500d7c530212115fb539d56d8e5
jidefine <youjio2000@naver.com>
2024-01-26 11:04:29 +0900

    Resolve15 tutor 풀이 추가

--- Ch03_Control/src/ch03/for10/Resolve14.java
+++ Ch03_Control/src/ch03/for10/Resolve14.java
@@ -5,19 +5,26 @@ import java.util.Scanner;
 public class Resolve14 {
 
 	public static void main(String[] args) {
-		
+
 		int num;
 		int result;
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.print("구구단 中 몇 단? >> ");
 		num = sc.nextInt();
 
-		for(int i =1;i<= 9;) { // ;을 통해서 한눈에 볼 수 있도록 사용
-			result= i * num; // 증가
-			System.out.println(num + " * "+ i + " = " + result); // 실행
+		for (int i = 1; i <= 9;) { // ;을 통해서 한눈에 볼 수 있도록 사용
+			result = i * num; // 증가
+			System.out.println(num + " * " + i + " = " + result); // 실행
 			i++;
 		}
+
+//		tutor 풀이 추가
+//		for(int i=1;i<=9;i++)
+//			System.out.printf("%d * %d = %d\n", 
+//								num, i, num*i);
+		
+//		ctrl + shift + F => 코드 정렬
 	}
 
 }
