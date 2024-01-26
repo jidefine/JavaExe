adc3982c47d6686e0f80ef8697032b9cb89f77df
jidefine <youjio2000@naver.com>
2024-01-26 10:09:41 +0900

    Resolve3_while(true)를 이용한 풀이방법 추가

--- Ch03_Control/src/ch03/for10/Resolve2.java
+++ Ch03_Control/src/ch03/for10/Resolve2.java
@@ -7,6 +7,7 @@ public class Resolve2 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.print("1~99까지의 정수 입력 >> ");
 		int num = sc.nextInt();
 		
@@ -40,6 +41,29 @@ public class Resolve2 {
 //			if(num2==3 | num2==6 | num2==9)
 //				System.out.println("박수짝");
 //		}
+		
+//		풀이방법 3)
+//		while(true) {
+//			
+//			System.out.print("1~99까지의 정수 입력 >> ");
+//			int num = sc.nextInt();
+//			if(num < 0)
+//				break;
+//			
+//			int num1 = num / 10;
+//			int num2 = num % 10;
+//			
+//			int count = 0;
+//			if(num1==3 | num1==6 | num1==9 )
+//				count++;
+//			if(num2==3 | num2==6 | num2==9)
+//				count++;
+//			if(count==1)
+//				System.out.println("박수짝");
+//			if(count==2)
+//				System.out.println("박수짝짝");
+//		}
+//		System.out.println("End~");
 
 	}
 
