975cb87d5e08f67b71060458950839c3635d7875
jidefine <youjio2000@naver.com>
2024-01-26 17:24:07 +0900

    배열 정렬

---
+++ Ch05_Array/src/ch05/array07/ArrayEx.java
@@ -0,0 +1,22 @@
+package ch05.array07;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/* 배열의 정렬
+ * Array.sort();
+ * Array.parallelSort();
+ */
+
+public class ArrayEx {
+
+	public static void main(String[] args) {
+		int[] scores = {100, 88, 895, 67, 75};
+		System.out.println(Arrays.toString(scores));
+		
+		// 정렬
+		Array.sort(scores);
+		
+	}
+
+}
