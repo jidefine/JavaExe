0f12e960c17e55e2dc9d8263c43bb624e608d581
jidefine <youjio2000@naver.com>
2024-01-29 10:58:15 +0900

    ch05.array04 / Resolve1 배열 항목 반복을 위한 for문 설명 추가

--- Ch05_Array/src/ch05/array05/ch05/sec09/AdvancedForExample.java
+++ Ch05_Array/src/ch05/array05/ch05/sec09/AdvancedForExample.java
@@ -5,9 +5,18 @@ public class AdvancedForExample {
 	public static void main(String[] args) {
 		int[] scores = {95, 71, 84, 93, 87};
 		int sum = 0;
-		for(int score : scores) { //모르겠음
-			sum = sum + score;
+		for(int score : scores) { // 5개의 항목이 한 번씩 score 변수에
+			sum = sum + score;    // 저장되고 sum에 누적됨(반복 횟수 5)
 		}
+		
+//		원래는 아래와 같으나, 편의상 위의 방식으로 코드 작성
+//		for(int i = 0;i<scores.length;i++) { 
+//		sum += scores[i];
+
+//		[배열 항목 반복을 위한 향상된 for문]
+//		for(타입 변수 : 배열){
+//		실행문
+//		}
 		
 		System.out.println("점수 총합 = " + sum);
 		
