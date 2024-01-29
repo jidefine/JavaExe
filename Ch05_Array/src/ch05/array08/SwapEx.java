ce6c4f4a3aa65a2fff3d1396bbde849fca46980c
jidefine <youjio2000@gmail.com>
2024-01-29 12:13:39 +0900

    Swap 예제

---
+++ Ch05_Array/src/ch05/array08/SwapEx.java
@@ -0,0 +1,29 @@
+package ch05.array08;
+
+public class SwapEx {
+	public static void main(String[] args) {
+		/*2개 변수의 값을 바꾸려면
+		 * Python은 아래처럼 가능
+		 * a = 10; 
+		 * b = 20; 
+		 * a, b = b, a;
+		 */
+		
+		int a = 100;
+		int b = 200;
+		a = b;
+		b = a;
+		System.out.printf("a=%d, b=%d", a, b);
+		System.out.println();
+		
+		int num0 = 10;
+		int num1 = 20;
+		int temp = 0;
+		
+		temp = num0;
+		num0 = num1;
+		num1 = temp;
+		System.out.printf("num0=%d, num1=%d", num0, num1);
+		
+	}
+}
