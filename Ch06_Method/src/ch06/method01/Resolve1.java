b38407038f42318cf9470bb3ddcdd5670871c8f7
jidefine <youjio2000@gmail.com>
2024-01-29 16:32:34 +0900

    ch06.method01 / Resolve1 문제해결

---
+++ Ch06_Method/src/ch06/method01/Resolve1.java
@@ -0,0 +1,45 @@
+package ch06.method01;
+
+import java.util.Scanner;
+
+/* 1. 2개의 정수를 입력받고 큰 값을 반환하는 메서드와
+   작은 값을 반환하는 메서드를 작성하고 사용하세요.
+*/
+public class Resolve1 {
+	public static int max(int num0, int num1) {
+		int result = 0;
+		if(num0 < num1)
+			result = num1;
+		else
+			result = num0;
+		return result;
+	}
+	
+	public static int min(int num0, int num1) {
+		int result = 0;
+		if(num0 < num1)
+			result = num0;
+		else
+			result = num1;
+		
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("1 번째 정수 입력 >> ");
+		int num0 = sc.nextInt();
+		System.out.print("2 번째 정수 입력 >> ");
+		int num1 = sc.nextInt();
+		
+		int max = max(num0, num1);
+		int min = min(num0, num1);
+		
+		System.out.println("큰 값 : " + max);
+		System.out.println("작은 값 : " + min);
+		
+		sc.close();
+	}
+
+}
