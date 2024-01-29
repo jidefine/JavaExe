e8691485f0fc281a59350289f87fe1e4a9a41e62
jidefine <youjio2000@naver.com>
2024-01-29 10:13:40 +0900

    ch03.continue11 / Resovle2 다시 풀어봐야 함

--- Ch03_Control/src/ch03/continue11/Resovle2.java
+++ Ch03_Control/src/ch03/continue11/Resovle2.java
@@ -1,13 +1,20 @@
 package ch03.continue11;
+
 // 다시 풀어봐야 함
+// for문을 이용해서 아래 출력
+//*
+//**
+//***
+//****
+//*****
 public class Resovle2 {
 	public static void main(String[] args) {
 
-		for (int i = 1; i <= 5; i++) {
-			for(int j=1; j<=i; j++) {
+		for (int i = 1; i <= 5; i++) {     //가로행j의 변화
+			for (int j = 1; j <= i; j++) { //세로열i이 같음
 				System.out.print("*");
 			}
-		System.out.println();
+			System.out.println();
 		}
 
 	}
