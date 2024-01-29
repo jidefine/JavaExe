1c49e279c7430fd986214f5d599c7ae3361224b6
jidefine <youjio2000@gmail.com>
2024-01-29 16:42:39 +0900

    ch06.method01 / Resolve2 문제해결

---
+++ Ch06_Method/src/ch06/method01/Resolve2.java
@@ -0,0 +1,33 @@
+package ch06.method01;
+
+import java.util.Scanner;
+
+/* 2. 두 개의 정수를 받아서, 두 수의 차의 절대값을 계산하여 출력하는 메서드를 만들고
+ 사용해보세요. 메서드 호출 시 전달되는 값의 순서와 상관없이
+절대값이 계산되어서 출력되어야 합니다
+*/
+public class Resolve2 {
+	public static int value(int num0, int num1) {
+		int result = num0 - num1;
+		if(result<0)
+			result = -(result);
+
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("1 번째 정수 입력 >> ");
+		int num0 = sc.nextInt();
+		System.out.print("2 번째 정수 입력 >> ");
+		int num1 = sc.nextInt();
+		
+		int value = value(num0, num1);
+		
+		System.out.println("두 수의 차의 절대값 : " + value);
+		
+		sc.close();
+	}
+
+}
