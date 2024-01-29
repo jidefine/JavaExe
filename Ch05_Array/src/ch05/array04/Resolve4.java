6d14e6a2da5356da7f4b08ed73faa58dac6cbf93
jidefine <youjio2000@gmail.com>
2024-01-29 11:17:20 +0900

    ch05.array04 / Resolve4 문제 풀이 오류 확인 및 tutor 풀이 추가

--- Ch05_Array/src/ch05/array04/Resolve4.java
+++ Ch05_Array/src/ch05/array04/Resolve4.java
@@ -3,9 +3,15 @@ package ch05.array04;
 public class Resolve4 {
 	public static void main(String[] args) {
 		
-		int[] num = {3, 6, 9, 12, 15, 18, 21, 24, 27, 30};
+//		int[] num = {3, 6, 9, 12, 15, 18, 21, 24, 27, 30};
 		
-		for(int i=9;i>=0;i--)
+		int[] num = new int[10]; //tutor 풀이 추가
+		
+		for(int i=0; i<num.length;i++) {
+			num[i] = 3*(i+1);
+		}
+		
+		for(int i=num.length-1;i>=0;i--)
 			System.out.print(num[i] + " ");
 		System.out.println();
 
