eeb265368e16f9feb45cbe4ded70f34dedfd3b97
jidefine <youjio2000@gmail.com>
2024-01-29 16:16:53 +0900

    ch06.method01 / Resolve5 문제 설명 변경

--- Ch06_Method/src/ch06/method01/Resolve5.java
+++ Ch06_Method/src/ch06/method01/Resolve5.java
@@ -1,8 +1,13 @@
 package ch06.method01;
 
-/* 3. int result = total(10, 100);
-위처럼 사용하면 result에는 10부터 100까지의 누적합이 저장되어야 합니다
-total메서드를 작성하고 사용해보세요.
+/* 5. star(5);
+   을 입력하면 
+   *
+   **
+   ***
+   ****
+   *****
+   위처럼 출력되는 void star(int num) 메서드를 작성하고 사용해보세요.
 */
 public class Resolve5 {
 	public static void star(int num) {
