eac138f763a0ffd144afb11470deca06211271ca
jidefine <youjio2000@naver.com>
2024-01-29 10:05:30 +0900

    ch03.continue11 / Resovle1 지역변수와 스택과 힙과의 관계

--- Ch03_Control/src/ch03/continue11/Resovle1.java
+++ Ch03_Control/src/ch03/continue11/Resovle1.java
@@ -4,13 +4,12 @@ import java.util.Scanner;
 
 public class Resovle1 {
 	public static void main(String[] args) {
-//	for(int i=0;i<=5;i++)
-//		System.out.println(i);
-//	
-		int sum = 0;
 
-		for (int i = 1; i <= 100;) {
-			if (i % 3 == 0)
+		int sum = 0; // stack에 들어감
+					 // sum은 main 메서드의 지역변수
+
+		for (int i = 1; i <= 100;) { // {}는 지역변수를 의미함
+			if (i % 3 == 0)          // 해당 영역 {}을 벗어나면 지역변수는 stack에서 사라짐
 				System.out.println(i);
 			sum += i;
 			i++; // 증감식(변화: 얼마만큼)
