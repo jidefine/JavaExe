31dcf42ea23a326a43f54bd0f5f2c73428725d87
jidefine <youjio2000@gmail.com>
2024-01-29 11:07:28 +0900

    ch05.array04 / Resolve2 tutor 풀이보고 수정 =eng.length-1

--- Ch05_Array/src/ch05/array04/Resolve2.java
+++ Ch05_Array/src/ch05/array04/Resolve2.java
@@ -5,7 +5,7 @@ public class Resolve2 {
 		
 		String[] eng = new String[] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
 		
-		for(int i=9;i>=0;i--)
+		for(int i=eng.length-1;i>=0;i--) // tutor 풀이보고 수정 =eng.length-1
 			System.out.print(eng[i] + " ");
 		System.out.println();
 		
