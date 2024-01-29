b347c72b6c1f2f08daddf40c9cec1d4a95e62a67
jidefine <youjio2000@naver.com>
2024-01-29 10:42:00 +0900

    ch05.array04 / Resolve1 배열 항목 반복을 위한 for문 복습 필요

--- Ch05_Array/src/ch05/array04/Resolve1.java
+++ Ch05_Array/src/ch05/array04/Resolve1.java
@@ -1,15 +1,19 @@
 package ch05.array04;
-
+// 1. String 배열을 10명의 이름으로 초기화하고 순서대로 출력하세요
 public class Resolve1 {
 	public static void main(String[] args) {
 		
-		String[] eng = new String[] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+		String[] names = new String[] {"a", "b", "c", "d", 
+									"e", "f", "g", "h", "i", "j"};
 		
-		for(int i=0;i<eng.length;i++)
-			System.out.print(eng[i] + " ");
+		for(int i=0;i<names.length;i++)
+			System.out.print(names[i] + " ");
 		System.out.println();
 		
-		
+//		강사 풀이 추가 1)
+		for(String name : names) // 교재 189쪽 참고
+			System.out.print(name + ", ");
+		System.out.println();
 
 	}
 }
