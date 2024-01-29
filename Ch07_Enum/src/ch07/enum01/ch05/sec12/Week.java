28c26a226842e18f48e489ff36fe0cb9991181a8
jidefine <youjio2000@gmail.com>
2024-01-29 17:41:17 +0900

    eunm 작성 예시

---
+++ Ch07_Enum/src/ch07/enum01/ch05/sec12/Week.java
@@ -0,0 +1,13 @@
+package ch07.enum01.ch05.sec12;
+//enum: 프로그래밍에 도움이 되는 기능을 가짐
+//프로그래밍의 가독성을 높이는데 쓰이는 문법
+//내부적으로는 차례대로 0부터 1씩 증가는 값을 가지고 있음?
+public enum Week {
+	MONDAY,// 내부적으로는 각각 숫자를 배정
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+}
