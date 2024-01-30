3c8970f2aeeeda9c3ee6c57ae51251f8b3960011
jidefine <youjio2000@gmail.com>
2024-01-30 12:29:11 +0900

    ch07.enum01.ch05.sec12 / Menu 인터페이스

---
+++ Ch07_Enum/src/ch07/enum01/ch05/sec12/Menu.java
@@ -0,0 +1,13 @@
+package ch07.enum01.ch05.sec12;
+
+//인퍼테이스의 용법 중에는 상수를 만들 때 사용하는 것도 있다.
+//상수는 일반적으로 대문자로 쓰는 것이 관습이다.
+public interface Menu {
+	int ZZATANGMYUN = 1; // 상수라는 것을 나타내기 위해 변수를 대문자로 작성
+	int ZZATPONG = 2;
+	int TANGSUYUK = 3;
+	int YONGANSUL = 4;
+	int YOULINGI = 5;
+	
+	
+}
