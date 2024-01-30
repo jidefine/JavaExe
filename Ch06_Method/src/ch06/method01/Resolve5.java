d4b6d198ff89019918fccbe200bf5aa69a8844df
jidefine <youjio2000@gmail.com>
2024-01-30 12:14:57 +0900

    ch06.method01 / Resolve5 주석 추가

--- Ch06_Method/src/ch06/method01/Resolve5.java
+++ Ch06_Method/src/ch06/method01/Resolve5.java
@@ -11,9 +11,9 @@ package ch06.method01;
 */
 public class Resolve5 {
 	public static void star(int num) {
-		
+		//가로행과 세로열의 * 수가 같음
 		for (int i = 1; i <= num; i++) {     //가로행j의 변화
-			for (int j = 1; j <= i; j++) { //세로열i이 같음
+			for (int j = 1; j <= i; j++) { //세로열i의 변화
 				System.out.print("*");
 			}
 			System.out.println();
