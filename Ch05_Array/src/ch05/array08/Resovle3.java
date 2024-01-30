fa52cf3bc014b1ceae2c56f8259a6769103651b2
jidefine <youjio2000@gmail.com>
2024-01-30 10:15:04 +0900

    ch05.array08 / Resovle3 포기

--- Ch05_Array/src/ch05/array08/Resovle3.java
+++ Ch05_Array/src/ch05/array08/Resovle3.java
@@ -1,7 +1,10 @@
 package ch05.array08;
 
 import java.util.Arrays;
-// 문제 해결 못함
+// 3. 정수 배열 5개를 할당하고
+//System.out.println(Arrays.toString(numArr));
+//로 배열을 출력했을 때 거꾸로 출력되도록
+//배열의 값을 거꾸로 저장하세요.
 public class Resovle3 {
 
 	public static void main(String[] args) {
@@ -9,25 +12,23 @@ public class Resovle3 {
 		int[] numArr = { 10, 99, 2, 78, 32 };
 		int i, j, k, temp;
 
-		for (i = numArr.length; i >= 0; i--) {
+		for (i = 0; i < numArr.length; i++) {
+			// 변수 k는 현재 정렬되지 않은 부분 배열의 길이
 			k = numArr.length - i;
-			for (j = k; j < 0; j--) {
-				if (numArr[j] < numArr[j - 1]) {
-					temp = numArr[j];
-					numArr[j] = numArr[j - 1];
-					numArr[j - 1] = temp;
+			for (j = 1; j < k; j++) {
+				if (numArr[j - 1] > numArr[j]) {
+					// 임시 변수 temp를 사용하여 두 값을 교환하기 전에 이전 값을 저장
+					temp = numArr[j - 1];
+					numArr[j - 1] = numArr[j];
+					// 이전 값을 저장한 임시 변수의 값을 할당하여 교환
+					numArr[j] = temp;
 				}
 			}
 
 		}
-//			
-//		for(int i=eng.length-1;i>=0;i--) // tutor 풀이보고 수정 =eng.length-1
-//			System.out.print(eng[i] + " ");
-		System.out.println();
-
+		
 		System.out.println(Arrays.toString(numArr));
-//		System.out.println("최소값 : " + num[0]);
-//		System.out.println("최대값 : " + num[4]);
+
 	}
 
 }
