6421fb8bd3f24b98a3bc5aa219f184cf3e989bd1
jidefine <youjio2000@gmail.com>
2024-01-31 15:24:06 +0900

    ch09.extends01 ExtendsMain 상속의 이해

---
+++ Ch09_Extends/src/ch09/extends01/Father.java
@@ -0,0 +1,7 @@
+package ch09.extends01;
+
+public class Father extends Grandfather{
+	void wealth() {
+		System.out.println("돈을 많이 벌었다");
+	}
+}
---
+++ Ch09_Extends/src/ch09/extends01/Grandfather.java
@@ -0,0 +1,7 @@
+package ch09.extends01;
+
+public class Grandfather {
+void handsome() {
+	System.out.println("잘생겼다~");
+}
+}
