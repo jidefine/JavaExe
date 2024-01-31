f0993390d9de5a3b5426152821c44a79aa8c0687
jidefine <youjio2000@gmail.com>
2024-01-31 11:13:47 +0900

    ch08.class09 / answer2 문제 해결

---
+++ Ch08_Class/src/ch08/class09/answer2.java
@@ -0,0 +1,35 @@
+package ch08.class09;
+
+import java.util.Scanner;
+
+public class answer2 {
+	// 필드
+	private String[] foods;
+	private int selectNum;
+	private Scanner sc;
+	private String name;
+	
+	public answer2(Scanner sc, String name, String[] foods) {
+		this.sc = sc;
+		this.name = name;
+		this.foods = foods;
+	}
+	
+	public void viewMenu() {
+		System.out.println("-----[" + this.name + "]-----");
+		for(int i=0;i<foods.length;i++) {
+			System.out.println(i + "." + foods[i]);
+		}
+	}
+	public void selectFood() {
+		System.out.println("-----[" + this.name + "]-----");
+		System.out.print("어떤 음식을 선택하시겠습니까? >> ");
+		selectNum = sc.nextInt();
+	}
+	public void deliveryFood() {
+		System.out.println("-----[" + this.name + "]-----");
+		String food = foods[selectNum];
+		System.out.println(food + "이 나왔습니다.");
+		System.out.println("맛있게 드세요 ^^");
+	}
+}
