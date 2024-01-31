ef40d36986189fe9dbd7d77b79a78d5ce8ebc1d6
jidefine <youjio2000@gmail.com>
2024-01-31 15:34:04 +0900

    ch09.extends02 / 필드 또한 상속이 된다.

---
+++ Ch09_Extends/src/ch09/extends02/Father.java
@@ -0,0 +1,8 @@
+package ch09.extends02;
+
+public class Father extends Grandfather{
+	long money = 1000000000000000L;//L 붙이면 long타입 됨
+	void wealth() {
+		System.out.println("돈을 많이 벌었다");
+	}
+}
---
+++ Ch09_Extends/src/ch09/extends02/Grandfather.java
@@ -0,0 +1,8 @@
+package ch09.extends02;
+
+public class Grandfather {
+	int handxomeScore = 10;
+	void handsome() {
+		System.out.println("잘생겼다~");
+	}
+}
