8e41f2493732f05d9f35b889d75dcafa9bb41cee
jidefine <youjio2000@gmail.com>
2024-02-01 17:06:17 +0900

    ch09.abstract09 / 추상 클래스 예제

---
+++ Ch09_Extends/src/ch09/abstract09/Add.java
@@ -0,0 +1,8 @@
+package ch09.abstract09;
+
+public class Add extends Calc{
+	@Override
+	public int calculate() {
+		return a+b;
+	}
+}
---
+++ Ch09_Extends/src/ch09/abstract09/Div.java
@@ -0,0 +1,8 @@
+package ch09.abstract09;
+
+public class Div extends Calc{
+	@Override
+	public int calculate() {
+		return a/b;
+	}
+}
