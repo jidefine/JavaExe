d1add3260c5e1752b0aec961f7d04c43af589b50
jidefine <youjio2000@gmail.com>
2024-02-02 16:00:06 +0900

    ch09.interface12 / StarMain 코드 설명 추가

--- Ch09_Extends/src/ch09/interface12/StarMain.java
+++ Ch09_Extends/src/ch09/interface12/StarMain.java
@@ -14,7 +14,9 @@ public class StarMain {
 	
 	
 public static void main(String[] args) {
-	StarUnit[] units = new StarUnit[] {
+	// StarUnit 클래스 내에서 배열을 생성하여 StarUnit의 자식(클래스)객체들을 저장하고, 
+	// 이 배열을 units 변수에 할당
+	StarUnit[] units = new StarUnit[] { 
 			new Marine(),
 			new Scv(),
 			new Hydra()
