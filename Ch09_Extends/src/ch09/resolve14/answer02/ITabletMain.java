a2bc69f8d9246fb13db65ce716e44819b85b0395
jideifne <youjio2000@gmail.com>
2024-02-04 22:11:50 +0900

    package ch09.resolve14.answer02 / ITabletMain 작성완료

--- Ch09_Extends/src/ch09/resolve14/answer02/ITabletMain.java
+++ Ch09_Extends/src/ch09/resolve14/answer02/ITabletMain.java
@@ -9,6 +9,45 @@ package ch09.resolve14.answer02;
    OEM을 의뢰한 비트캠프는 TabletTester를 생성합니다.
    이 클래스들이 완성되면 TabletMain에서 테스트를 진행하세요.
    */
-public class ITabletMain {
+import java.util.Arrays;
 
+import ch09.resolve14.answer02.company.LG;
+import ch09.resolve14.answer02.company.Samsung;
+import ch09.resolve14.answer02.company.Sony;
+import ch09.resolve14.answer02.ITabletTester;
+
+public class ITabletMain {
+ public static void main(String[] args) throws InterruptedException {
+	ITabletTester itabletTester = new ITabletTester();
+	ITablet[] tabletArr = new ITablet[] { new Samsung(), new Sony(), new LG()};
+	int[] score = new int[tabletArr.length];
+	
+	for(int i=0;i<tabletArr.length;i++) {
+		itabletTester.setTablet(tabletArr[i]);
+		
+		score[i] += itabletTester.movieTest();
+		System.out.println("=> movieTest score : " +  score[i]);
+		score[i] += itabletTester.musicTest();
+		System.out.println("=> musicTest score : " +  score[i]);
+		score[i] += itabletTester.readBookTest();
+		System.out.println("=> readBookTest score : " +  score[i]);
+		
+		System.out.println("-------------------------------");
+		System.out.println("total score : " + score[i]);
+		System.out.println("-------------------------------");
+		System.out.println();
+	}
+	
+	// 가장 큰 점수를 받은 값과 인덱스 찾기
+	int max = score[0];
+	int maxIdx = 0;
+	for(int i=1;i<score.length;i++) {
+		if(max < score[i]) {
+			max = score[i];
+			maxIdx = i;
+		}
+	}
+	System.out.printf("가장 높은 점수를 받은 태블릿은 %d번째 태블릿이고 점수는 %d이다\n", 
+																maxIdx, max);
+}
 }
--- Ch09_Extends/src/ch09/resolve14/answer02/ITabletTester.java
+++ Ch09_Extends/src/ch09/resolve14/answer02/ITabletTester.java
@@ -1,16 +1,20 @@
 package ch09.resolve14.answer02;
 
-import ch09.interface13.ICar;
+import ch09.resolve14.answer02.ITablet;
 
 public class ITabletTester {
 	private final int TEST_NUM = 3; //테스트 횟수 3
 	private ITablet tablet; //tablet은 ITablet 인터페이스를 구현한 객체를 참조하는 변수
 	
-	 // ITablet 인터페이스를 구현한 객체를 전달받아 변수 tablet에 할당
+	 // 생성자를 생성하여, ITablet 인터페이스를 구현한 객체를 전달받아 변수 tablet에 할당
 	public ITabletTester(ITablet tablet) {
 		this.tablet = tablet;
 	}
-	
+	// 바로 위의 생성자로 인해 Java가 자동으로 디폴트 생성자를 추가하지 않아서 작성해 줌
+	public ITabletTester() {
+		
+	}
+
 	public void setTablet(ITablet tablet) { //ITablet 타입의 매개변수 tablet을 받음
 		this.tablet = tablet; // ITabletTester 클래스 내의 tablet 변수에 할당
 	}
