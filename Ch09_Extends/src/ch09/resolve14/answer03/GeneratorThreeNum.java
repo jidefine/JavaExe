f78b0d1b51e04ec9462c91f52df5507a94c70860
ADMIN <ADMIN@14.33.80.68>
2024-02-04 23:14:55 +0900

    ch09.resolve14.answer03 / GeneratorThreeNum 난수 3개를 제대로 생성하는지 확인

--- Ch09_Extends/src/ch09/resolve14/answer03/GeneratorThreeNum.java
+++ Ch09_Extends/src/ch09/resolve14/answer03/GeneratorThreeNum.java
@@ -3,14 +3,14 @@ package ch09.resolve14.answer03;
 import ch09.resolve14.answer03.ThreeNum;
 
 public class GeneratorThreeNum implements ThreeNum {
-	private int num1;
-	private int num2;
-	private int num3;
+	public int num1;
+	public int num2;
+	public int num3;
 	@Override
 	public void num1() throws InterruptedException {
 		// Math.random을 사용해서 0.0~10.0 미만의 실수를 랜덤으로 생성
 		// 실수형의 난수를 생성하기 때문에 정수형으로 변환
-		int num1 = (int)(Math.random() * 10); 
+		this.num1 = (int)(Math.random() * 10); 
 		System.out.println("num1 : " + num1);
 		
 	}
@@ -18,7 +18,7 @@ public class GeneratorThreeNum implements ThreeNum {
 	@Override
 	public void num2() throws InterruptedException {
 		do {
-			int num2 = (int)(Math.random() * 10);
+			this.num2 = (int)(Math.random() * 10);
 		}while(num2 == num1); // num2과 num1가 같으면 다시 난수 생성
 		System.out.println("num2 : " + num2);
 	}
@@ -26,10 +26,19 @@ public class GeneratorThreeNum implements ThreeNum {
 	@Override
 	public void num3() throws InterruptedException {
 		do {
-			int num3 = (int)(Math.random() * 10);
+			this.num3 = (int)(Math.random() * 10);
 		}while(num3 == num1 && num3 == num2); // num3과 num1, 2가 같으면 다시 난수 생성
 		System.out.println("num3 : " + num3);
 	}
     
-    
+//	public static void main(String[] args) {
+//		GeneratorThreeNum run = new GeneratorThreeNum();
+//		try {
+//            run.num1();
+//            run.num2();
+//            run.num3();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//	}
 }
