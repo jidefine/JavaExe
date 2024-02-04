98d40d431f4ce79186675e6c0d321c36c490cf29
ADMIN <ADMIN@14.33.80.68>
2024-02-05 00:58:38 +0900

    ch09.resolve14.answer03 / DecisionBall 수정

--- Ch09_Extends/src/ch09/resolve14/answer03/DecisionBall.java
+++ Ch09_Extends/src/ch09/resolve14/answer03/DecisionBall.java
@@ -1,5 +1,37 @@
 package ch09.resolve14.answer03;
 
 public class DecisionBall {
-
+	int[] nums;
+	int[] userNums;
+	
+	public int[] decision(GeneratorThreeNum2 generatorThreeNum2,
+			              BaseBallMenu baseBallMenu) {
+		int[] nums = generatorThreeNum2.nums();
+		int[] userNums = baseBallMenu.userNums();
+	}
+	
+	public void result() {
+		while(true) {
+			int strike = 0;
+			int ball = 0;
+			if(nums[0] == userNums[0] 
+					&& nums[1] == userNums[1] 
+							&& nums[2] == userNums[2])
+				break;
+				
+			for(int i =0; i<nums.length; i++) {
+				for(int j = 0; j <nums.length; j++) {
+					if(i == j && nums[i] == userNums[j]) {
+						strike++;
+					}
+					else if(nums[i] == userNums[j]) {
+						ball++;
+					}
+					
+				}
+			}
+			System.out.printf("%d스트라이크 %d볼\n", strike, ball);
+		}
+		System.out.println("==> 삼진아웃!!");
+	}
 }
