aaa0297542df49c3b9c1e5009d34491c5af18d3e
ADMIN <ADMIN@14.33.80.68>
2024-02-05 00:59:27 +0900

    ch09.resolve14.answer03 / BaseBallMain 수정

--- Ch09_Extends/src/ch09/resolve14/answer03/BaseBallMain.java
+++ Ch09_Extends/src/ch09/resolve14/answer03/BaseBallMain.java
@@ -2,6 +2,10 @@ package ch09.resolve14.answer03;
 
 public class BaseBallMain {
 	public static void main(String[] args) {
-
+		GeneratorThreeNum2 nums = new GeneratorThreeNum2();
+		BaseBallMenu userNums = new BaseBallMenu();
+		DecisionBall result = new DecisionBall();
+		
+		result.result();
 	}
 }
