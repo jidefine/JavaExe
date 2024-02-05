5c5f79159d61c5d0e1deec3757631ad71701528d
jidefine <youjio2000@gmail.com>
2024-02-05 11:43:36 +0900

    ch09.interface12 / StarMain 중 for(StarUnit unit : units) 부분에 대한 설명 추가

--- Ch09_Extends/src/ch09/interface12/StarMain.java
+++ Ch09_Extends/src/ch09/interface12/StarMain.java
@@ -3,6 +3,11 @@ package ch09.interface12;
 public class StarMain {
 	public static void walk(StarUnit[] units) {
 		for(StarUnit unit : units) {
+			/*StarUnit 클래스에서 unit이라는 지역변수를 선언하고,
+			* units 배열의 각 요소를 unit에 할당하여 반복
+			*/
+			// unit은 현재 반복에서의 현재 요소를 나타내는 지역 변수
+			// StarUnit 객체의 배열인 units
 			// unit에 어떤 객체가 들어가느냐에 따라 해당 객체의 walk()가 실행됨
 			unit.walk();
 		}
