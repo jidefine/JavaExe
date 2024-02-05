ffb963e2d1935415e138a1668200861ac5340a6d
jidefine <youjio2000@gmail.com>
2024-02-05 09:32:48 +0900

    ch09.resolve14.answer03 / GeneratorThreeNum2 난수 생성 후 촐력하는 코드를 주석처리

--- Ch09_Extends/src/ch09/resolve14/answer03/GeneratorThreeNum2.java
+++ Ch09_Extends/src/ch09/resolve14/answer03/GeneratorThreeNum2.java
@@ -14,7 +14,7 @@ public class GeneratorThreeNum2 implements ThreeNum {
 		// Math.random을 사용해서 0.0~10.0 미만의 실수를 랜덤으로 생성
 		// 실수형의 난수를 생성하기 때문에 정수형으로 변환
 		this.nums[0] = (int)(Math.random() * 10); 
-		System.out.println("num1 : " + nums[0]);
+//		System.out.println("num1 : " + nums[0]);
 		
 	}
 
@@ -23,7 +23,7 @@ public class GeneratorThreeNum2 implements ThreeNum {
 		do {
 			this.nums[1] = (int)(Math.random() * 10);
 		}while(nums[1] == nums[0]); // num2과 num1가 같으면 다시 난수 생성
-		System.out.println("num2 : " + nums[1]);
+//		System.out.println("num2 : " + nums[1]);
 	}
 
 	@Override
@@ -31,7 +31,7 @@ public class GeneratorThreeNum2 implements ThreeNum {
 		do {
 			this.nums[2] = (int)(Math.random() * 10);
 		}while(nums[2] == nums[1] && nums[2] == nums[0]); // num3과 num1, 2가 같으면 다시 난수 생성
-		System.out.println("num3 : " + nums[2]);
+//		System.out.println("num3 : " + nums[2]);
 	}
     
 //	public static void main(String[] args) {
