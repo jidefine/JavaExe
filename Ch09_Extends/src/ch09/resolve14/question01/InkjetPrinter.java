711462c2b9a3b2da1f3a6e5f1787974131d5d31a
jidefine <youjio2000@gmail.com>
2024-02-05 13:37:37 +0900

    ch09.resolve14.question01 / inkReminders 오류 수정

--- Ch09_Extends/src/ch09/resolve14/question01/InkjetPrinter.java
+++ Ch09_Extends/src/ch09/resolve14/question01/InkjetPrinter.java
@@ -2,11 +2,17 @@ package ch09.resolve14.question01;
 
 public class InkjetPrinter extends Printer {
 
-	protected int inkReminders;
+	protected int inkReminders;// 현재 InkjetPrinter 클래스의 객체(변수)
 	
+	// InkjetPrinter 클래스의 생성자 생성
 	public InkjetPrinter(String modelName, String manufacturer, P_INTERFACE pInterface, int printCount,
-			int paperRemains) {
+			int paperRemains, int inkRemainders, int inkReminders) { // 생성자는 6개의 매개변수를 받음
+		// InkjetPrinter 클래스가 상속하는 상위 클래스의 생성자를 호출
 		super(modelName, manufacturer, pInterface, printCount, paperRemains);
+		/* 객체가 생성될 때
+		* .inkReminders을 inkReminders 매개변수 값(입력된 값)으로 할당하여 초기화
+		*/
+		// this.inkReminders는 현재 InkjetPrinter 클래스의 객체(변수)
 		this.inkReminders = inkReminders;
 	}
 
@@ -14,7 +20,7 @@ public class InkjetPrinter extends Printer {
 	public void print() {
 		super.print();	// 종이 1장 사용
 		System.out.println("잉크가 잘 분사되어 출력된다.");
-		System.out.println();
+		System.out.println("품질이 양호하다.");
 	}
 
 	@Override
