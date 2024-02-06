879d023b14d0dbfeddf046f58660944433221e89
jidefine <youjio2000@gmail.com>
2024-02-06 10:43:37 +0900

    ch10.collection01.ch15 / 나머지 에제 작성 완료

--- Ch10_Collection/src/ch10/collection01/ch15/sec04/exam03/PropertiesExample.java
+++ Ch10_Collection/src/ch10/collection01/ch15/sec04/exam03/PropertiesExample.java
@@ -4,7 +4,7 @@ import java.util.Properties;
 
 public class PropertiesExample {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		//Properties 컬랙션 생성
 		Properties properties  = new Properties();
 		
