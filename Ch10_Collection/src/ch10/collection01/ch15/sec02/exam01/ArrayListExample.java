2e2408fcb8f53b5bc2c23b77b449214db20a1c7b
jidefine <youjio2000@gmail.com>
2024-02-06 13:26:49 +0900

    ch10.collection01.ch15.sec02.exam01 / ArrayListExample 향상된 for문으로 모든 객체를 하나씩 가져오기

--- Ch10_Collection/src/ch10/collection01/ch15/sec02/exam01/ArrayListExample.java
+++ Ch10_Collection/src/ch10/collection01/ch15/sec02/exam01/ArrayListExample.java
@@ -5,34 +5,39 @@ import java.util.List;
 
 public class ArrayListExample {
 	public static void main(String[] args) {
-		//ArrayList 컬렉션 생성
+		// ArrayList 컬렉션 생성
+		// <>에 자료형 대신 클래스를 넣어서 객체를 생성할 수 있음
 		List<Board> list = new ArrayList<>();
-		
-		//객체 추가
+
+		// 객체 추가
 		list.add(new Board("제목1", "내용1", "글쓴이1"));
 		list.add(new Board("제목2", "내용2", "글쓴이2"));
 		list.add(new Board("제목3", "내용3", "글쓴이3"));
 		list.add(new Board("제목4", "내용4", "글쓴이4"));
 		list.add(new Board("제목5", "내용5", "글쓴이5"));
-		
-		//저장된 총 객체 수 얻기
+
+		// 저장된 총 객체 수 얻기
 		int size = list.size();
 		System.out.println("총 객체 수: " + size);
 		System.out.println();
-		
-		//특정 인뎃으의 객체 가져오기
+
+		// 특정 인뎃으의 객체 가져오기
 		Board board = list.get(2);
-		System.out.println(board.getSubject() 
-				+ "\t" + board.getContent() 
-				+ "\t" + board.getWriter());
+		System.out.println(board.getSubject() + "\t" + board.getContent() 
+												+ "\t" + board.getWriter());
 		System.out.println();
-		
-		//모든 객체 하나씩 가져오기
-		for(int i=0; i<list.size(); i++) {
+
+		// 모든 객체 하나씩 가져오기
+		for (int i = 0; i < list.size(); i++) {
 			Board b = list.get(i);
-			System.out.println(b.getSubject() 
-					+ "\t" + b.getContent() 
-					+ "\t" + b.getWriter());
+			System.out.println(b.getSubject() + "\t" + b.getContent() 
+												+ "\t" + b.getWriter());
+		}
+
+		// 향상된 for문으로 모든 객체를 하나씩 가져오기
+		for (Board b : list) {
+			System.out.println(b.getSubject() + "\t" + b.getContent() 
+												+ "\t" + b.getWriter());
 		}
 	}
 
