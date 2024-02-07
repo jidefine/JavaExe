1929f0e6b18cc0650eb438a4f8e48dba9edd6cb6
jidefine <youjio2000@gmail.com>
2024-02-07 15:59:49 +0900

    ch11.string02.ch12.sec05 / ByteToStringExample 주석 추가

--- Ch11_String/src/ch11/string02/ch12/sec05/ByteToStringExample.java
+++ Ch11_String/src/ch11/string02/ch12/sec05/ByteToStringExample.java
@@ -8,7 +8,7 @@ public class ByteToStringExample {
 		String data = "자바";
 		
 		//String -> byte 배열(기본: UTF-8인코딩)
-		byte[] arr1 = data.getBytes();
+		byte[] arr1 = data.getBytes(); // utf-8,data.getBytes("UTF-8")
 		System.out.println("arr1" + Arrays.toString(arr1));
 		
 		//byte 배열 -> String(기본: UTF-8인코딩)
@@ -20,6 +20,7 @@ public class ByteToStringExample {
 		System.out.println("arr2" + Arrays.toString(arr2));
 		
 		//byte 배열 -> String(기본: UTF-8인코딩)
+		// 상대방이 보낸 파일이 다를 때는 명확하게 byte 단위를 UTF-8으로 변경? 
 			String str2 = new String(arr2, "EUC-KR");
 			System.out.println("str2: " + str2);
 		
