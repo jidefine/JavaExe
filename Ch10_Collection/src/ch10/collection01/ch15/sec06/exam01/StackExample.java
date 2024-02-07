f870596f63f149e28f9d130f62cadf428b7f407d
jidefine <youjio2000@gmail.com>
2024-02-07 10:06:17 +0900

    ch10.collection01.ch15.sec06.exam01 / StackExample 주석 추가

--- Ch10_Collection/src/ch10/collection01/ch15/sec06/exam01/StackExample.java
+++ Ch10_Collection/src/ch10/collection01/ch15/sec06/exam01/StackExample.java
@@ -8,14 +8,14 @@ public class StackExample {
 		Stack<Coin> coinBox = new Stack<Coin>();
 		
 		//동전 넣기
-		coinBox.push(new Coin(100));
+		coinBox.push(new Coin(100)); // 스택에 넣음
 		coinBox.push(new Coin(50));
 		coinBox.push(new Coin(500));
 		coinBox.push(new Coin(10));
 
 		//동전을 하나씩 꺼내기
 		while(!coinBox.isEmpty()) {
-			Coin coin = coinBox.pop();
+			Coin coin = coinBox.pop(); // 스택에서 빼옴
 			System.out.println("꺼내온 동전 : " + coin.getValue() + "원");
 		}
 	}
