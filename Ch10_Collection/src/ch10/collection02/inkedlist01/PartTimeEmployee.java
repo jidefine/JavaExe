fdf2e8b60d39cdbaf6cd29a0f04878837d4b9b79
jidefine <youjio2000@gmail.com>
2024-02-07 11:18:57 +0900

    ch10.collection02.hashmap04 / 문제 1 생성

---
+++ Ch10_Collection/src/ch10/collection02/inkedlist01/PartTimeEmployee.java
@@ -0,0 +1,30 @@
+package ch10.collection02.inkedlist01;
+
+public class PartTimeEmployee extends Employee {
+	
+	protected int dailyPay; // 일당
+	protected int workDay; // 일한 일수
+	
+	public PartTimeEmployee(String empno, String name,
+			int dailyPay, int workDay) {
+		super(empno, name);
+		this.dailyPay = dailyPay;
+		this.workDay = workDay;
+	}
+
+	@Override
+	public double getMonthPay() {
+		double mPay = dailyPay*workDay;
+		return mPay;
+	}
+	
+	@Override
+	public void showEmployeeInfo() { // 부모의 기능 + 추가 확장
+		super.showEmployeeInfo(); // Employee의 showEmployeeInfo() 샐행
+		System.out.println("dailyPay : " + dailyPay);
+		System.out.println("workDay : " + workDay);
+		System.out.println("monthPay : " + getMonthPay());
+		
+	}
+
+}
