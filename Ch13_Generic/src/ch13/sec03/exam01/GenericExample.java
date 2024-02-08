6f3188416b89eafbff3d526bd9d61839abd16efd
jidefine <youjio2000@gmail.com>
2024-02-08 11:38:47 +0900

    ch13.sec03.exam01 / GenericExample 주석 추가

--- Ch13_Generic/src/ch13/sec03/exam01/GenericExample.java
+++ Ch13_Generic/src/ch13/sec03/exam01/GenericExample.java
@@ -2,21 +2,24 @@ package ch13.sec03.exam01;
 
 public class GenericExample {
 	//제네릭 메소드
+	//<T> : 다팁 파라미터
+	//Box<T> : return 자료형
+	//t : 매개변수
 	public static <T> Box<T> boxing(T t) {
 		Box<T> box = new Box<T>();
 		box.set(t);
-		return box;
+		return box; 
 	}
 
 	public static void main(String[] args) {
 		//제네릭 메소드 호출
 		Box<Integer> box1 = boxing(100);
 		int intValue = box1.get();
-		System.out.println(intValue);
+		System.out.println(intValue); // Integer 형을 return
 
 		//제네릭 메소드 호출
 		Box<String> box2 = boxing("홍길동");
 		String strValue = box2.get();
-		System.out.println(strValue);
+		System.out.println(strValue); // String 형을 return
 	}
 }
