c6c39cf8d19450668083b42c882578eacdc51faa
jidefine <youjio2000@gmail.com>
2024-02-08 17:07:00 +0900

    ExceptionHandlingExample1 주석 추가

--- Ch14_Exception/src/ch14/exception01/ch11/sec02/exam01/ExceptionHandlingExample1.java
+++ Ch14_Exception/src/ch14/exception01/ch11/sec02/exam01/ExceptionHandlingExample1.java
@@ -9,7 +9,7 @@ public class ExceptionHandlingExample1 {
 	public static void main(String[] args) {
 		System.out.println("[프로그램 시작]\n");
 		printLength("ThisIsJava");
-		printLength(null);
+		printLength(null); // null은 가리키는 값이 없음
 		System.out.println("[프로그램 종료]");
 		
 	}
