1e69f4401d56b2730efdf46a1c143895ff61aea4
jidefine <youjio2000@gmail.com>
2024-02-08 18:18:11 +0900

    ch10.collection02.inkedlist03 강사 풀이

---
+++ Ch10_Collection/src/ch10/collection02/inkedlist01/tutor/TempEmployee.java
@@ -0,0 +1,31 @@
+package ch10.collection02.inkedlist01.tutor;
+
+public class TempEmployee extends Employee {
+
+	protected int yearSalary; // 연봉
+	protected int hireYear;      // 계약기간
+	
+	public TempEmployee(String empno, String name,
+			int yearSalary, int hireYear) {
+		super(empno, name);
+		this.yearSalary = yearSalary;
+		this.hireYear = hireYear;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double getMonthPay() {
+		double mPay = (double)yearSalary/12;
+		return mPay;
+	}
+
+	@Override
+	public void showEmployeeInfo() {
+		// TODO Auto-generated method stub
+		super.showEmployeeInfo();
+		System.out.println("yearSalary : " + yearSalary);
+		System.out.println("hireYear : " + hireYear);
+		System.out.println("monthPay : " + getMonthPay());
+	}
+	
+}
