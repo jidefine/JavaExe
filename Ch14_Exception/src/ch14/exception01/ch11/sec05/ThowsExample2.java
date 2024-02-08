641c6f2c5302de8646afd2d7a35f932700f95131
jidefine <youjio2000@gmail.com>
2024-02-08 14:02:07 +0900

    ch14.exception01.ch11.sec05 / ThowsExample2 클래스 생성

---
+++ Ch14_Exception/src/ch14/exception01/ch11/sec05/ThowsExample2.java
@@ -0,0 +1,10 @@
+package ch14.exception01.ch11.sec05;
+
+public class ThowsExample2 {
+public static void main(String[] args) throws Exception{
+	findClass();
+}
+public static void findClass() throws ClassNotFoundException{
+	Class.forName("java.lang.String2");
+}
+}
