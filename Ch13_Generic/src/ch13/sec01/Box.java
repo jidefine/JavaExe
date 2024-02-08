49bd43193a020c4418359a70e2cc8cf884208e8a
jidefine <youjio2000@gmail.com>
2024-02-08 11:20:47 +0900

    ch13.sec01 / Box

--- Ch13_Generic/src/ch13/sec01/Box.java
+++ Ch13_Generic/src/ch13/sec01/Box.java
@@ -1,5 +1,10 @@
 package ch13.sec01;
-
-public class Box<T> {
+/*T는 대명사이다
+ * E, A, MY, HELLO ... 모두 가능하다.
+ * T는 자료형을 프로그래머가 직접 선택하라는 의미
+ * 다만 T는 (Type)을 의미
+ * 
+ */
+public class Box<T> { 
 	public T content;
 }
