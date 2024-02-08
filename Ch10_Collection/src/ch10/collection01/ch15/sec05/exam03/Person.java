2c6e002a3242697e6472169e22a417dab8d01f3f
jidefine <youjio2000@gmail.com>
2024-02-08 17:47:08 +0900

    ch10.collection01.ch15.sec05.exam03 / Person 오류 수정

--- Ch10_Collection/src/ch10/collection01/ch15/sec05/exam03/Person.java
+++ Ch10_Collection/src/ch10/collection01/ch15/sec05/exam03/Person.java
@@ -1,23 +1,24 @@
 package ch10.collection01.ch15.sec05.exam03;
 
-public class Person implements Comparable<Person>{
+public class Person implements Comparable<Person> {
 	public String name;
 	public int age;
+
 	public Person(String name, int age) {
-	this.name = name;
-	this.age = age;
+		this.name = name;
+		this.age = age;
 	}
+//	@Override
+//	public int compareTo(Person o) {
+//	// 원하는 값으로 정렬 가능
+//	if(age<o.age) return -1; //부호를 반대로 바꾸면 반대로 정렬됨
+//	else if(age == o.age) return 0;
+//	else return 1;
+
 	@Override
-	public int compareTo(Person o) {
-	// 원하는 값으로 정렬 가능
-	if(age<o.age) return -1; //부호를 반대로 바꾸면 반대로 정렬됨
-	else if(age == o.age) return 0;
-	else return 1;
-	
 	public int compareTo(Person o) {
 		return name.compareTo(o.name);
 		// return name.compareTo(o.name)*-1;
 	}
 
-	}
 }
