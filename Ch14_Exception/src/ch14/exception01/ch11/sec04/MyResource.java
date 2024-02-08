c1325823ce76cbd202d25769191cdabe6d177c11
jidefine <youjio2000@gmail.com>
2024-02-08 12:47:40 +0900

    ch14.exception01.ch11.sec04 / MyResource 클래스 생성

---
+++ Ch14_Exception/src/ch14/exception01/ch11/sec04/MyResource.java
@@ -0,0 +1,22 @@
+package ch14.exception01.ch11.sec04;
+
+public class MyResource interface AutoCloseable{
+private String name;
+
+public MyResource(String name) {
+	this.name = name;
+	System.out.println("[MyResource(" + name + ") 읽기]");
+}
+public String read1() {
+	System.out.println("[MyResource(" + name + ") 읽기]");
+	return "100";
+}
+public String read2() {
+	System.out.println("[MyResource(" + name + ") 읽기]");
+	return "abc";
+}
+@Override
+public void close() throws Excaption{
+	System.out.println("[MyResource(" + name + ") 닫기]");
+}
+}
