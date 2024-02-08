e1fcb1f9167e3d50d682151a76f7190f1a9ace38
jidefine <youjio2000@gmail.com>
2024-02-08 17:12:29 +0900

    ExceptionHandlingExample 주석 추가

--- Ch14_Exception/src/ch14/exception01/ch11/sec03/exam01/ExceptionHandlingExample.java
+++ Ch14_Exception/src/ch14/exception01/ch11/sec03/exam01/ExceptionHandlingExample.java
@@ -6,7 +6,10 @@ public class ExceptionHandlingExample {
 		
 		for(int i=0; i<=array.length; i++) {
 			try {
-				int value = Integer.parseInt(array[i]);
+				// 문자열을 Integer로 변환 중 2개의 오류 발생
+				// 세밀하게 에외처리를 해줄 때 사용
+				// 귀찮으면 그냥 Exception 으로 처리하면 모든 예외를 받을 수 있음
+				int value = Integer.parseInt(array[i]); 
 				System.out.println("array["+i+"]: "+ value);
 			} catch(ArrayIndexOutOfBoundsException e) {
 				System.out.println("배열 인덱스가 초과됨: " + e.getMessage());
