f714e5abe09d0388bae68e5d3a4d7718b07faee4
jidefine <youjio2000@gmail.com>
2024-02-08 17:13:25 +0900

    ExceptionHandlingExample 주석 추가

--- Ch14_Exception/src/ch14/exception01/ch11/sec03/exam02/ExceptionHandlingExample.java
+++ Ch14_Exception/src/ch14/exception01/ch11/sec03/exam02/ExceptionHandlingExample.java
@@ -8,9 +8,9 @@ public class ExceptionHandlingExample {
 			try {
 				int value = Integer.parseInt(array[i]);
 				System.out.println("array["+i+"]: "+ value);
-			} catch(ArrayIndexOutOfBoundsException e) {
+			} catch(ArrayIndexOutOfBoundsException e) { // 예외는 순서대로 처리함
 				System.out.println("배열 인덱스가 초과됨: " + e.getMessage());
-			} catch(Exception e) {
+			} catch(Exception e) { // 모든 예외를 다 받을 수 있는 부모 클래스임
 				System.out.println("숫자로 변환할 수 없음: "+e.getMessage());
 			}
 			
