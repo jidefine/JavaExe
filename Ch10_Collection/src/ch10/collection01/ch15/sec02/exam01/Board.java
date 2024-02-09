cb12019b53ee4fed8597791f98bf2c7ce4d28f5f
jidifine <youjio2000@gmail.com>
2024-02-09 23:16:22 +0900

    ch10.collection01.ch15.sec02.exam01 / Board 복습

--- Ch10_Collection/src/ch10/collection01/ch15/sec02/exam01/Board.java
+++ Ch10_Collection/src/ch10/collection01/ch15/sec02/exam01/Board.java
@@ -1,20 +1,35 @@
 package ch10.collection01.ch15.sec02.exam01;
 
 public class Board {
-	private String subject;
-	private String content;
-	private String writer;
+	private String subject; //필드 생성
+	private String content; //필드 생성
+	private String writer;  //필드 생성
 	
+	// 매개변수 subject, content, writer를 갖는 Board 객체 생성자 생성
 	public Board(String subject, String content, String writer) {
-	this.subject = subject;
-	this.content = content;
-	this.writer = writer;
+		// Board 객체의 필드들을 매개변수로 받은 값으로 초기화
+		this.subject = subject;
+		this.content = content;
+		this.writer = writer;
 	}
-	
-	public String getSubject() {return subject;}
-	public void getSubject(String subject) {this.subject = subject;}
-	public String getContent() {return content;}
-	public void setContent(String content) {this.content = content;}
-	public String getWriter() {return writer;}
-	public void setWriter(String writer) {this.writer = writer;}
+	// Board 객체의 subject 필드 값을 String형으로 반환하는 getSubject 메서드
+	public String getSubject() {
+		return subject; // 
+	}
+	// Board 객체의 subject 필드 값을 설정하는 메서드
+	public void getSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
 }
