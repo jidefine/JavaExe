b058caa6fa8314c9b68fa7884f44790202d520ed
jidefine <youjio2000@gmail.com>
2024-02-13 16:58:31 +0900

    ch15.thread01.ch14.sec07.exam01 스레드 안전 종료

--- Ch15_Thread/src/ch15/thread01/ch14/sec07/exam01/PrintThread.java
+++ Ch15_Thread/src/ch15/thread01/ch14/sec07/exam01/PrintThread.java
@@ -6,10 +6,16 @@ public class PrintThread extends Thread {
 	public void setStop(boolean stop) {
 		this.stop = stop;
 	}
-
+	/*[스레드를 종료하는 방법]
+	 * 1) 게속 동작해야 하는 스레드에서 while문을 종종 사용\
+	 * 이 스레드를 종료하기 위해서는 아래처럼 boolean stop을 사용하면
+	 * stop이 true일 때 이상 만복되지 않고 종료
+	 * printThread.setStop(true);
+	 * 2) 
+	 */
 	@Override
 	public void run() {
-		while(!stop) {
+		while(!stop) { // false
 			System.out.println("실행 중");
 		}
 		System.out.println("리소스 정리");
--- Ch15_Thread/src/ch15/thread01/ch14/sec07/exam01/SafeStopExample.java
+++ Ch15_Thread/src/ch15/thread01/ch14/sec07/exam01/SafeStopExample.java
@@ -1,5 +1,5 @@
 package ch15.thread01.ch14.sec07.exam01;
-
+// 스레드의 졸료
 public class SafeStopExample {
 	public static void main(String[] args) {
 		PrintThread printThread = new PrintThread();
