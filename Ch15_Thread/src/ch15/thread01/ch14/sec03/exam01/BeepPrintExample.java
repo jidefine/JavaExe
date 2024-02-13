5e0bd542b5354c2d8be1673a1e7188dafb7b68fc
jidefine <youjio2000@gmail.com>
2024-02-13 12:12:43 +0900

    ch15.thread01.ch14.sec03.exam01 / BeepPrintExample 주석 추가

--- Ch15_Thread/src/ch15/thread01/ch14/sec03/exam01/BeepPrintExample.java
+++ Ch15_Thread/src/ch15/thread01/ch14/sec03/exam01/BeepPrintExample.java
@@ -1,17 +1,25 @@
 package ch15.thread01.ch14.sec03.exam01;
 
-import java.awt.Toolkit;
+// 
+import java.awt.Toolkit; // UI 만들때 사용하는 라이브러리
 
 public class BeepPrintExample {
-public static void main(String[] args) {
-	Toolkit toolkit = Toolkit.getDefaultToolkit(); // Toolkit 객체 얻기
-	for(int i=0; i<5; i++) {
-		toolkit.beep(); //비프음 발생
-		try {Thread.sleep(500); } catch(Exception e) {}
+	public static void main(String[] args) {
+		Toolkit toolkit = Toolkit.getDefaultToolkit(); // Toolkit 객체 얻기
+		for (int i = 0; i < 5; i++) {
+			toolkit.beep(); // 비프음 발생
+			// sleep : cpu에게 제어권을 주지마(실행하지마)
+			try {
+				Thread.sleep(500);
+			} catch (Exception e) {
+			}
+		}
+		for (int i = 0; i < 5; i++) {
+			System.out.println("띵");
+			try {
+				Thread.sleep(500);
+			} catch (Exception e) {
+			}
+		}
 	}
-	for(int i=0; i <5; i++) {
-		System.out.println("띵");
-		try {Thread.sleep(500); } catch(Exception e) {}
-	}
-}
 }
