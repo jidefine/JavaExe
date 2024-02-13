76001e0f23c40d44388e1dee79f2e3952025bf9c
jidefine <youjio2000@gmail.com>
2024-02-13 11:27:46 +0900

    ch15.thread01.ch14.sec05.exam03 /

---
+++ Ch15_Thread/src/ch15/thread01/ch14/sec05/exam03/WorkThread.java
@@ -0,0 +1,24 @@
+package ch15.thread01.ch14.sec05.exam03;
+
+public class WorkThread extends Thread{
+	// 필드
+	public boolean work = true;
+	
+	// 생성자
+	public WorkThread(String name) {
+		setName(name);
+	}
+	
+	// 메소드
+	@Override
+	public void run() {
+		while(true) {
+			if(work) {
+				System.out.println(getName() + ": 작업 처리");
+			} else {
+				Thread.yield();
+			}
+		}
+	}
+	
+}
