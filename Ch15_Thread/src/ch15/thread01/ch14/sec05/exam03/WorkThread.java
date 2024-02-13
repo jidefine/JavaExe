c07965078bf587d0db51fa15d50f94f0e0914e3f
jidefine <youjio2000@gmail.com>
2024-02-13 14:48:28 +0900

    ch15.thread01.ch14.sec05.exam03.WorkThread 주석 추가

--- Ch15_Thread/src/ch15/thread01/ch14/sec05/exam03/WorkThread.java
+++ Ch15_Thread/src/ch15/thread01/ch14/sec05/exam03/WorkThread.java
@@ -8,7 +8,17 @@ public class WorkThread extends Thread{
 	public WorkThread(String name) {
 		setName(name);
 	}
-	
+	/* 스레즈의 제어 권한을 다른 스레드에 넘기는 방법
+	 * 1) sleep(10;
+	 * 10ms 동안 현재 스레드는 스케쥴링에서 제외하겠음
+	 * 즉, 다른 스레드에 실행을 넘길꺼임
+	 * 난 멈추겠음(입력한 시간 만큼 아무것도 안 할꺼임)
+	 * 2) Thread.yield();
+	 * 의미없는 일을 하느리 즉시 넘기겠음
+	 * 현재 스레드는 스케쥴링에서 넘길꺼임
+	 * 즉, 다른 스레드에 실행을 넘길꺼임
+	 * 
+	 */
 	// 메소드
 	@Override
 	public void run() {
@@ -16,6 +26,7 @@ public class WorkThread extends Thread{
 			if(work) {
 				System.out.println(getName() + ": 작업 처리");
 			} else {
+				// cpu의 사용 권한(스케쥴링)을 다른 스레드에 넘김
 				Thread.yield();
 			}
 		}
