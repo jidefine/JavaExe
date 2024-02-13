dbdec21d6d815e7bbba46c44d758b60397ebe8d8
ADMIN <ADMIN@221.150.180.181>
2024-02-13 21:16:45 +0900

    ch16.sec01.Calculable

--- Ch16_lambda/src/ch16/lambda01/ch16/sec01/Calculable.java
+++
@@ -1,5 +0,0 @@
-package ch16.lambda01.ch16.sec01;
-
-public class Calculable {
-
-}
---
+++ Ch16_lambda/src/ch16/lambda02/ch16/sec01/Calculable.java
@@ -0,0 +1,7 @@
+package ch16.lambda02.ch16.sec01;
+
+@FunctionalInterface
+public interface Calculable {
+	//추상 메소드
+		void calculate(int x, int y);
+}
