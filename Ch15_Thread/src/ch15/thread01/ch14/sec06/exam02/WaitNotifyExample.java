dd165d6fe57b47105fd8eb1a001756230c1e16cb
jidefine <youjio2000@gmail.com>
2024-02-13 16:32:37 +0900

    ch15.thread01.ch14.sec06.exam01.WaitNotifyExample 주석 추가

--- Ch15_Thread/src/ch15/thread01/ch14/sec06/exam02/WaitNotifyExample.java
+++ Ch15_Thread/src/ch15/thread01/ch14/sec06/exam02/WaitNotifyExample.java
@@ -1,5 +1,7 @@
 package ch15.thread01.ch14.sec06.exam02;
-
+/* 순서를 맞춰주는
+ * 
+ */
 public class WaitNotifyExample {
 	public static void main(String[] args) {
 		WorkObject workObject = new WorkObject();
