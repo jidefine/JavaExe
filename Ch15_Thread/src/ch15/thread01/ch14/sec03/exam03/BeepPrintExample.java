783a2af246b769fa5be71c1babfce95583ad9aac
jidefine <youjio2000@gmail.com>
2024-02-13 12:33:38 +0900

    ch15.thread01.ch14.sec03.exam03 / BeepPrintExample 주석 수정

--- Ch15_Thread/src/ch15/thread01/ch14/sec03/exam03/BeepPrintExample.java
+++ Ch15_Thread/src/ch15/thread01/ch14/sec03/exam03/BeepPrintExample.java
@@ -3,8 +3,11 @@ package ch15.thread01.ch14.sec03.exam03;
 import java.awt.Toolkit;
 
 public class BeepPrintExample {
+//
 public static void main(String[] args) {
+	// main tread(주인 아저씨)가 work tread(알바생)을 고용
 	Thread thread = new Thread() {
+		// 알바생이 해야 할일
 		@Override
 		public void run(){
 			Toolkit toolkit = Toolkit.getDefaultToolkit();
