ec43be9ab33542fd60ceac55464983d31af18f52
jidefine <youjio2000@gmail.com>
2024-02-13 16:31:29 +0900

    ch15.thread01.ch14.sec06.exam01.Calculator 주석 추가

--- Ch15_Thread/src/ch15/thread01/ch14/sec06/exam01/Calculator.java
+++ Ch15_Thread/src/ch15/thread01/ch14/sec06/exam01/Calculator.java
@@ -1,12 +1,14 @@
 package ch15.thread01.ch14.sec06.exam01;
 
 public class Calculator {
-	private int memory;
+	// 이 변수를 2개의 스레드가 동시에  접근할 가능성이 있기 때문에 스레드 동기호시켜줌
+	private int memory; 
 
 	public int getMeomory() {
 		return memory;
 	}
 	// 동기화 메서드
+	// 동기화를 안한다고 해서 오류가 꼭 나는 것은 아님
 	public synchronized void setMonryl(int memory) {
 		this.memory = memory; // 메모리 값 저장
 		try {
