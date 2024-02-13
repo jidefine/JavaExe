a0be920ab8f01c6d988e6c6efc231b05fb1e5b03
jidefine <youjio2000@gmail.com>
2024-02-13 14:15:51 +0900

    ch15.thread01.ch14.sec04.ThreadNameExample 주석 추가

--- Ch15_Thread/src/ch15/thread01/ch14/sec04/ThreadNameExample.java
+++ Ch15_Thread/src/ch15/thread01/ch14/sec04/ThreadNameExample.java
@@ -3,16 +3,18 @@ package ch15.thread01.ch14.sec04;
 public class ThreadNameExample {
 	public static void main(String[] args) {
 	Thread mainThread = Thread.currentThread();
-	System.out.println(mainThread.getName() + "실행");
+	// 내재되어 있는 main 스레드의 기본 이름인 'main'으로 출력
+	System.out.println(mainThread.getName() + "실행"); 
 	
 	for(int i=0; i<3; i++) {
 		Thread threadA = new Thread() {
 		@Override
 		public void run() {
-			System.out.println(getName() + "실행");
+			// 내재되어 있는 스레드 기본 이름으로 출력
+			System.out.println(getName() + "실행"); 
 		}
 		};
-		threadA.start();
+		threadA.start(); // 스레드 실행
 	}
 	Thread chatThread = new Thread() {
 		@Override
@@ -20,7 +22,7 @@ public class ThreadNameExample {
 			System.out.println(getName() + "실행");
 		}
 	};
-	chatThread.setName("chat-thread");
+	chatThread.setName("chat-thread"); // 스레드 이름 변경 가능
 	chatThread.start();
 	}
 }
