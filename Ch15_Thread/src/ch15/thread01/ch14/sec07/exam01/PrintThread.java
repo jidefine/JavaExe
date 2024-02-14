ec8193b9f7c58aacbca0f05c95f8eb8594a6a911
jidefine <youjio2000@gmail.com>
2024-02-14 09:51:10 +0900

    ch15.thread01.ch14.sec07.exam01PrintThread 주석 수정

--- Ch15_Thread/src/ch15/thread01/ch14/sec07/exam01/PrintThread.java
+++ Ch15_Thread/src/ch15/thread01/ch14/sec07/exam01/PrintThread.java
@@ -1,18 +1,17 @@
 package ch15.thread01.ch14.sec07.exam01;
-
+/*[스레드를 종료하는 방법]
+ * 1) 계속 동작해야 하는 스레드에서 while문을 종종 사용\
+ * 이 스레드를 종료하기 위해서는 아래처럼 boolean stop을 사용하면
+ * stop이 true일 때 이상 만복되지 않고 종료
+ * printThread.setStop(true);
+ * 2) 
+ */
 public class PrintThread extends Thread {
 	private boolean stop;
 
 	public void setStop(boolean stop) {
 		this.stop = stop;
 	}
-	/*[스레드를 종료하는 방법]
-	 * 1) 게속 동작해야 하는 스레드에서 while문을 종종 사용\
-	 * 이 스레드를 종료하기 위해서는 아래처럼 boolean stop을 사용하면
-	 * stop이 true일 때 이상 만복되지 않고 종료
-	 * printThread.setStop(true);
-	 * 2) 
-	 */
 	@Override
 	public void run() {
 		while(!stop) { // false
