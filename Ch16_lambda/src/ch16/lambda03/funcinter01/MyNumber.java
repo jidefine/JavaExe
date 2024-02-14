915f629720eb759915cbd1404719284cf5791582
jidefine <youjio2000@gmail.com>
2024-02-14 13:21:10 +0900

    ch16.lambda03 Lambda 예제 및 설명

--- Ch16_lambda/src/ch16/lambda03/funcinter01/MyNumber.java
+++ Ch16_lambda/src/ch16/lambda03/funcinter01/MyNumber.java
@@ -7,6 +7,5 @@ package ch16.lambda03.funcinter01;
 public interface MyNumber {
 	// 추상 메서드
 	// 람다식은 추상 메서드가 무조건 하나여야 한다
-	int getMax(int num1, int num2);
-	
+	int getMax(int num1, int num2);da03 Lambda 예제 및 설명)
 }
