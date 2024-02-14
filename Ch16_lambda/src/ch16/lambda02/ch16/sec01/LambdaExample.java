6674e6f7a54f1aa59e1450da3a95a5c5ad9e6a98
jidefine <youjio2000@gmail.com>
2024-02-14 14:45:52 +0900

    ch16.lambda03 람다식 예제 추가

--- Ch16_lambda/src/ch16/lambda02/ch16/sec01/LambdaExample.java
+++ Ch16_lambda/src/ch16/lambda02/ch16/sec01/LambdaExample.java
@@ -2,11 +2,13 @@ package ch16.lambda02.ch16.sec01;
 
 public class LambdaExample {
 	public static void main(String[] args) {
+		// (x, y) : 어떤 알파벳으로 지정을 하던 파라미터 갯수만 맞으면 됨
+		// 람다식이 2줄 이상이면, 중괄호를 쳐줘야 함
 		action((x, y) -> {
 			int result = x + y;
 			System.out.println("result: " + result);
 		});
-
+		// 파라미터가 2개 이상이면 괄호를 쳐줘야 함
 		action((x, y) -> {
 			int result = x - y;
 			System.out.println("result: " + result);
--- Ch16_lambda/src/ch16/lambda03/funcinter01/MyNumber.java
+++ Ch16_lambda/src/ch16/lambda03/funcinter01/MyNumber.java
@@ -1,7 +1,12 @@
 package ch16.lambda03.funcinter01;
-
+/* 추상메서드가 1개를 초과하면 에러가 발생함
+ * 함수형 인터페이스는 반드시 추상메서드가 1개여야 함
+ */
+//함수형 람다식을 위한 인터페이스
+@FunctionalInterface
 public interface MyNumber {
 	// 추상 메서드
 	// 람다식은 추상 메서드가 무조건 하나여야 한다
 	int getMax(int num1, int num2);
+	
 }
