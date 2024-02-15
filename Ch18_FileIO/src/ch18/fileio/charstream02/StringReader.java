c9ea5733a1af512307852550aef689671704395f
jidefine <youjio2000@gmail.com>
2024-02-15 15:04:12 +0900

    ch18.fileio.charstream02.StringReader 문자열 읽기

---
+++ Ch18_FileIO/src/ch18/fileio/charstream02/StringReader.java
@@ -0,0 +1,12 @@
+package ch18.fileio.charstream02;
+
+public class StringReader {
+	public static void main(String[] args) {
+		Reader rd = new FileReader("손흥민.txt");
+		BufferedReader in = new BufferedReader(rd);
+		String str;
+		while(true) {
+			str = in.
+		}
+	}
+}
