848c809feb3ebc441d201b2fd38071b91fc49702
jidefine <youjio2000@gmail.com>
2024-02-15 15:05:06 +0900

    ch18.fileio.charstream02.PrinterWriterStream PrintWriter를 활용 작성 중

--- Ch18_FileIO/src/ch18/fileio/charstream02/PrinterWriterStream.java
+++ Ch18_FileIO/src/ch18/fileio/charstream02/PrinterWriterStream.java
@@ -1,13 +1,15 @@
 package ch18.fileio.charstream02;
 
-import java.io.PrintWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.Writer;
 
-public class PrinterWriterStream {
+public class PrintWriterStream {
 	public static void main(String[] args) throws IOException {
 		Writer wt = new FileWriter("printf.txt");
+		
+		/* Console출력 메서드를 파일출력에 사용할 수 있게 제공해주는 필터 클래스*/
 		PrintWriter out = new PrintWriter(wt);
 		out.println("PrintWriter 사용법입니다");
 		out.printf("제 나이는 %d살입니다/n", 24);
