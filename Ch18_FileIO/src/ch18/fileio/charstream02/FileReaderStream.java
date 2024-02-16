b30cb200986c853175aaa2230e1d56c2eb63a59a
jidefine <youjio2000@gmail.com>
2024-02-16 10:50:06 +0900

    ch18.fileio.charstream02.FileReaderStream 문자스트림 파일 읽기

--- Ch18_FileIO/src/ch18/fileio/charstream02/FileReaderStream.java
+++ Ch18_FileIO/src/ch18/fileio/charstream02/FileReaderStream.java
@@ -1,28 +1,17 @@
 package ch18.fileio.charstream02;
 
-<<<<<<< HEAD
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 
 public class FileReaderStream {
 	public static void main(String[] args) throws IOException {
-=======
-import java.io.Reader;
-
-public class FileReaderStream {
-	public static void main(String[] args) {
->>>>>>> d17d304 (ch18.fileio.charstream02.FileReaderStream 문자스트림 파일 읽기)
 		char[] cBuf = new char[10];
 		int readCnt = 0;
 		Reader in = new FileReader("hyper.txt");
 		readCnt = in.read(cBuf, 0, cBuf.length);
-<<<<<<< HEAD
 		for(int i=0;i<readCnt; i++)
 			System.out.println(cBuf[i]);
 		in.close();
-=======
-		for(int i=0; )
->>>>>>> d17d304 (ch18.fileio.charstream02.FileReaderStream 문자스트림 파일 읽기)
 	}
 }
