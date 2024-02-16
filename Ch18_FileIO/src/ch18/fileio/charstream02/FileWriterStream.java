4b4b28b6d79dd9c1c2fdd9220805f770a88d41d1
jidefine <youjio2000@gmail.com>
2024-02-16 15:07:12 +0900

    ch19.server.webserver03.FileWriterStream 오류 수정

--- Ch18_FileIO/src/ch18/fileio/charstream02/FileWriterStream.java
+++ Ch18_FileIO/src/ch18/fileio/charstream02/FileWriterStream.java
@@ -1,16 +1,20 @@
 package ch18.fileio.charstream02;
 
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 
 /* 1) 바이크 스트림 : 있는 그대로 읽고, 있는 그대로 쓰기
  * 				메모리에 있는 데이터가 그대로 파일에 기록
  * 2) 문자 스트림 : 메모장으로 읽기 위한 텍스트 파일로
  * 				개행처리가 OS마다 다르다
  * 				Java의 클래스는 OS마다 알맞게 개행처리 해준다
- * 
+ * 				'\n' -> Unix/Linux : '\n'
+ *                      Windows : '\r\n'
+ *                      Mac : '\r'
  */
 public class FileWriterStream {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		char ch1 = 'A';
 		char ch2 = 'B';
 		
